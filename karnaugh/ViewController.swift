4e30fa6b95e41d1e00e9e3088c9e5545eece4a10
sdq <sdq1990@outlook.com>
2015-06-18 21:04:38 +0800

    new project

---
+++ karnaugh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  karnaugh
+//
+//  Created by sdq on 15/6/18.
+//  Copyright (c) 2015年 sdq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
